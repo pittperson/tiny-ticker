4c7d4530d53331cb12648c0b860668fa2963e161
Chris Lies <chris@chrislies.com>
2021-09-07 15:19:52 -0400

    added previous close

--- app.py
+++ app.py
@@ -17,13 +17,7 @@ def index():
 def ticker(symbol, domain):
     return render_template("ticker.html", symbol=symbol, domain=domain, finnhubKey=FINNHUB_KEY)
 
-# @app.route('/symbol/<symbol>')
-# def get_symbol(symbol):
-
-#     finnhub_client = finnhub.Client(api_key=FINNHUB_KEY)
-#     symbol_data = finnhub_client.quote(symbol.upper())
-
-#     return render_template("index.html", symbol=symbol, finnhubKey=FINNHUB_KEY)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000,debug=True)
+    # app.run(host="0.0.0.0",port=5000,debug=False)
